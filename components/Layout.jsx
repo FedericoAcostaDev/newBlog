6f2dbd37d960374f1e42de3bb985c6691278c0d6
Federico <fedeacos045@gmail.com>
2021-12-27 17:02:32 -0300

    added postsCard but w have the TypeError: Cannot read properties of undefined (reading 'edges') error

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,6 @@ import { Header } from "./Header";
 const Layout = ({ children }) => {
   return (
     <>
-      <br />
       <Header />
       {children}
     </>
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post} key={post.titulo} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -26,6 +26,7 @@ export default function Home({ posts }) {
   );
 }
 
+//fetching data on next.js (outside the component)
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
 
